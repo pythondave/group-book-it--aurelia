f2a4005a49bd408713a4eb1ff7293e47517ffc68
nialljt1 <nialltucker@gmail.com>
2017-02-28 11:53:20 +0000

    Updated text to "Example welcome4"

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ export class App {
     config.map([
       { route: ['', 'welcome'], name: 'welcome', moduleId: './components/welcome', nav: true, title: 'Welcome' },
       { route: 'bookings', name: 'welcome', moduleId: './components/bookings', nav: true, title: 'Bookings' },
-      { route: 'example-welcome', name: 'welcome', moduleId: './components/examples/welcome', nav: true, title: 'Example welcome3' },
+      { route: 'example-welcome', name: 'welcome', moduleId: './components/examples/welcome', nav: true, title: 'Example welcome4' },
       { route: 'example-users', name: 'users', moduleId: './components/examples/users', nav: true, title: 'Example users' },
       { route: 'example-child-router', name: 'child-router', moduleId: './components/examples/child-router', nav: true, title: 'Example child router' }
     ]);
